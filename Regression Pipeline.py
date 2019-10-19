7574fe7d4509041fe9f6d8e7cafa31748907415a
Fayaz <fayazsalim@gmail.com>
2019-10-19 19:54:19 +0530

    Added Regression Pipeline

---
+++ Regression Pipeline.py
@@ -0,0 +1,46 @@
+import lightgbm as lgb
+import numpy as np
+import pandas as pd
+import xgboost as xgb
+from sklearn.base import BaseEstimator, TransformerMixin, RegressorMixin, clone
+from sklearn.decomposition import PCA
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.kernel_ridge import KernelRidge
+from sklearn.linear_model import ElasticNet, Lasso
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler, RobustScaler
+
+
+class AveragingModels(BaseEstimator, RegressorMixin, TransformerMixin):
+    def __init__(self, models):
+        self.models_ = [clone(x) for x in self.models]
+        self.models = models
+
+    # we define clones of the original models to fit the data in
+    def fit(self, X, y):
+        # Train cloned base models
+        for model in self.models_:
+            model.fit(X, y)
+
+        return self
+
+    # Now we do the predictions for cloned models and average them
+    def predict(self, X):
+        predictions = np.column_stack([
+            model.predict(X) for model in self.models_
+        ])
+        return np.mean(predictions, axis=1)
+
+
+model_xgb = xgb.XGBRegressor()
+model_lgb = lgb.LGBMRegressor()
+lasso = make_pipeline(RobustScaler(), Lasso(alpha=0.0005, random_state=1))
+ENet = make_pipeline(RobustScaler(), ElasticNet(alpha=0.0005, l1_ratio=.9, random_state=3))
+KRR = KernelRidge(alpha=0.6, kernel='polynomial', degree=2, coef0=2.5)
+GBoost = GradientBoostingRegressor(n_estimators=3000, learning_rate=0.05,
+                                   max_depth=4, max_features='sqrt',
+                                   min_samples_leaf=15, min_samples_split=10,
+                                   loss='huber', random_state=5)
+
+averaged_models = AveragingModels(models=(ENet, GBoost, KRR, lasso, model_xgb, model_lgb))
