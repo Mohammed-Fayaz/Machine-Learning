b9aa22e56f60998f23da035a81de063b18adabcf
Fayaz <fayazsalim@gmail.com>
2019-10-20 12:47:11 +0530

    Added Tests For read()

--- Tests/regression_pipeline_tests.py
+++ Tests/regression_pipeline_tests.py
@@ -1,8 +1,26 @@
 import unittest
-from ..Templates import regression_pipeline
+import numpy as np
+import pandas as pd
+
+from Templates import regression_pipeline
 
 
 class TestRead(unittest.TestCase):
     """
     Tests the read function
     """
+
+    def setUp(self):
+        self.TRAIN_PATH = 'C:\\Users\\fayaz\\PycharmProjects\\Machine-Learning\\Tests\\train.csv'
+        self.TEST_PATH = 'C:\\Users\\fayaz\\PycharmProjects\\Machine-Learning\\Tests\\test.csv'
+
+    def tearDown(self):
+        pass
+
+    def test_if_read_works_alright(self):
+        X_train, y_train, X_test = regression_pipeline.read(train_path=self.TRAIN_PATH, test_path=self.TEST_PATH,
+                                                            label_name='d')
+
+
+if __name__ == '__main__':
+    unittest.main()
